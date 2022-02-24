0da1d1b4e3cb5fb8b448fa761082db9d341970c9
dinesh chandrasekaren <64305855+dineshchandrasekaren@users.noreply.github.com>
2022-02-24 18:01:38 +0530

    Update ExpressionCheckUsingStack.java

--- ExpressionCheckUsingStack.java
+++ ExpressionCheckUsingStack.java
@@ -5,13 +5,13 @@ import java.util.Stack;
 public class Expression {
     public Boolean isBalanced(String input){
         Stack<Character> stacks =new Stack<>();
-     
+
         for (char ch :
                 input.toCharArray()) {
-            if (ch == '(')
+            if (ch == '('|| ch=='['|| ch=='{'||ch=='<')
                 stacks.push(ch);
 
-            if(ch==')') {
+            if(ch==')'|| ch==']'||ch=='}'||ch=='>') {
                 if(stacks.empty()) return  false;
                 stacks.pop();
 
