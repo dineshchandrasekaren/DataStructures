640119e77ba8f71810e096ebc3af085e09363638
dinesh chandrasekaren <64305855+dineshchandrasekaren@users.noreply.github.com>
2022-02-25 11:51:36 +0530

    Update ExpressionCheckUsingStack.java

--- ExpressionCheckUsingStack.java
+++ ExpressionCheckUsingStack.java
@@ -1,22 +1,32 @@
-
-
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class Expression {
+        List<Character> leftBracket= Arrays.asList( '(',  '[',  '{', '<');
+        List<Character> rightBracket= Arrays.asList( ')',  ']',  '}', '>');
     public Boolean isBalanced(String input){
         Stack<Character> stacks =new Stack<>();
-
         for (char ch :
                 input.toCharArray()) {
-            if (ch == '('|| ch=='['|| ch=='{'||ch=='<')
+            if (isLeftBracket(ch))
                 stacks.push(ch);
 
-            if(ch==')'|| ch==']'||ch=='}'||ch=='>') {
-                if(stacks.empty()) return  false;
-                stacks.pop();
-
+            if(isRightBracket(ch)) {
+              var top=  stacks.pop();
+                if(isBracketMatch(ch,top)) return  false;
             }
+
         }
         return  stacks.empty();
+    }
+    private boolean isLeftBracket(char ch){
+        return this.leftBracket.contains(ch);
+    }
+    private boolean isRightBracket(char ch){
+        return this.leftBracket.contains(ch);
+    }
+    private boolean isBracketMatch(char left,char right){
+        return leftBracket.indexOf(left)==rightBracket.indexOf(right);
     }
 }
